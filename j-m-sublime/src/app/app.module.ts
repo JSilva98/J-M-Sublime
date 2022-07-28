a61fac3e4f50f9d3df47857e7b7b7d05128509d5
JPSilva98 <joao.silva@netcar.pt>
2022-07-29 00:05:39 +0100

    Project v0.1 - Page Layout (header + content + footer) added and with substantial progress made. Global variables added along with breakpoints.

--- j-m-sublime/src/app/app.module.ts
+++ j-m-sublime/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
